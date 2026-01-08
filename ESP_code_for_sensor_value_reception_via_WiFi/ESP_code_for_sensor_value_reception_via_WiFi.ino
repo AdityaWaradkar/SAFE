69284b2732e4e149b7b3b59a0627662eb5a073b6
Aditya Raman <adityaarvind3@gmail.com>
2026-01-09 01:08:46 +0530

    Add esp8266MOD 12E code common for all types of zones

--- ESP_code_for_sensor_value_reception_via_WiFi/ESP_code_for_sensor_value_reception_via_WiFi.ino
+++ ESP_code_for_sensor_value_reception_via_WiFi/ESP_code_for_sensor_value_reception_via_WiFi.ino
@@ -3,23 +3,18 @@
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 
-/* =========================================================
-   SELECT THE ROLE OF THIS ESP
-   ---------------------------------------------------------
-   Uncomment ONLY ONE of the following.
-   ========================================================= */
+#define USE_ROOMS // uncomment for rooms
+// #define USE_CORRIDORS          // uncomment for corridors
+// #define USE_CONFERENCE_ROOM    // uncomment for conference rooms
 
-#define USE_ROOMS
-// #define USE_CORRIDORS
-// #define USE_CONFERENCE_ROOM
-
-const char *ssid     = "BePositive";
+const char *ssid = "BePositive";
 const char *password = "Positive24";
 
 unsigned long lastSVGFetch = 0;
 const unsigned long SVG_INTERVAL = 15000;
 
-struct ZoneData {
+struct ZoneData
+{
   int flame;
   int smoke;
   float temperature;
@@ -28,24 +23,22 @@ struct ZoneData {
 #ifdef USE_ROOMS
 const char *apiUrl = "https://safe-0vvn.onrender.com/data/rooms";
 ZoneData rooms[6];
-const char* roomKeys[6] = {
-  "room1", "room2", "room3", "room4", "room5", "safeRoom"
-};
+const char *roomKeys[6] = {
+    "room1", "room2", "room3", "room4", "room5", "safeRoom"};
 #endif
 
 #ifdef USE_CORRIDORS
 const char *apiUrl = "https://safe-0vvn.onrender.com/data/corridors";
 ZoneData corridors[6];
-const char* corridorKeys[6] = {
-  "corridor1", "corridor2", "corridor3",
-  "corridor4", "corridor5", "corridor6"
-};
+const char *corridorKeys[6] = {
+    "corridor1", "corridor2", "corridor3",
+    "corridor4", "corridor5", "corridor6"};
 #endif
 
 #ifdef USE_CONFERENCE_ROOM
 const char *apiUrl = "https://safe-0vvn.onrender.com/data/conferenceRoom";
 ZoneData confRoom[2];
-const char* confKeys[2] = { "A", "B" };
+const char *confKeys[2] = {"A", "B"};
 #endif
 
 void setup()
@@ -56,7 +49,8 @@ void setup()
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
     yield();
@@ -75,13 +69,15 @@ void fetchSVGData()
   client.setInsecure();
 
   HTTPClient http;
-  if (!http.begin(client, apiUrl)) {
+  if (!http.begin(client, apiUrl))
+  {
     Serial.println("HTTP begin failed");
     return;
   }
 
   int httpCode = http.GET();
-  if (httpCode != HTTP_CODE_OK) {
+  if (httpCode != HTTP_CODE_OK)
+  {
     Serial.printf("HTTP error: %d\n", httpCode);
     http.end();
     return;
@@ -91,32 +87,34 @@ void fetchSVGData()
   http.end();
 
   StaticJsonDocument<1024> doc;
-  if (deserializeJson(doc, payload)) {
+  if (deserializeJson(doc, payload))
+  {
     Serial.println("JSON parse failed");
     return;
   }
 
-
   Serial.print("Timestamp: ");
-  Serial.println((const char*)doc["timestamp"]);
+  Serial.println((const char *)doc["timestamp"]);
 
   Serial.print("Received At: ");
-  Serial.println((const char*)doc["receivedAt"]);
+  Serial.println((const char *)doc["receivedAt"]);
 
 // ROOMS MODE
 #ifdef USE_ROOMS
   JsonObject roomsObj = doc["rooms"];
 
-  for (int i = 0; i < 6; i++) {
-    rooms[i].flame       = roomsObj[roomKeys[i]]["flame"];
-    rooms[i].smoke       = roomsObj[roomKeys[i]]["smoke"];
+  for (int i = 0; i < 6; i++)
+  {
+    rooms[i].flame = roomsObj[roomKeys[i]]["flame"];
+    rooms[i].smoke = roomsObj[roomKeys[i]]["smoke"];
     rooms[i].temperature = roomsObj[roomKeys[i]]["temperature"];
   }
 
   Serial.println("Room Data:");
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 6; i++)
+  {
     Serial.printf("%s -> F:%d S:%d T:%.2f\n",
-      roomKeys[i], rooms[i].flame, rooms[i].smoke, rooms[i].temperature);
+                  roomKeys[i], rooms[i].flame, rooms[i].smoke, rooms[i].temperature);
   }
 #endif
 
@@ -124,17 +122,19 @@ void fetchSVGData()
 #ifdef USE_CORRIDORS
   JsonObject corridorsObj = doc["corridors"];
 
-  for (int i = 0; i < 6; i++) {
-    corridors[i].flame       = corridorsObj[corridorKeys[i]]["flame"];
-    corridors[i].smoke       = corridorsObj[corridorKeys[i]]["smoke"];
+  for (int i = 0; i < 6; i++)
+  {
+    corridors[i].flame = corridorsObj[corridorKeys[i]]["flame"];
+    corridors[i].smoke = corridorsObj[corridorKeys[i]]["smoke"];
     corridors[i].temperature = corridorsObj[corridorKeys[i]]["temperature"];
   }
 
   Serial.println("Corridor Data:");
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 6; i++)
+  {
     Serial.printf("%s -> F:%d S:%d T:%.2f\n",
-      corridorKeys[i], corridors[i].flame,
-      corridors[i].smoke, corridors[i].temperature);
+                  corridorKeys[i], corridors[i].flame,
+                  corridors[i].smoke, corridors[i].temperature);
   }
 #endif
 
@@ -142,21 +142,22 @@ void fetchSVGData()
 #ifdef USE_CONFERENCE_ROOM
   JsonObject confObj = doc["conferenceRoom"];
 
-  for (int i = 0; i < 2; i++) {
-    confRoom[i].flame       = confObj[confKeys[i]]["flame"];
-    confRoom[i].smoke       = confObj[confKeys[i]]["smoke"];
+  for (int i = 0; i < 2; i++)
+  {
+    confRoom[i].flame = confObj[confKeys[i]]["flame"];
+    confRoom[i].smoke = confObj[confKeys[i]]["smoke"];
     confRoom[i].temperature = confObj[confKeys[i]]["temperature"];
   }
 
   Serial.println("Conference Room Data:");
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < 2; i++)
+  {
     Serial.printf("Conf %s -> F:%d S:%d T:%.2f\n",
-      confKeys[i], confRoom[i].flame,
-      confRoom[i].smoke, confRoom[i].temperature);
+                  confKeys[i], confRoom[i].flame,
+                  confRoom[i].smoke, confRoom[i].temperature);
   }
 #endif
 }
-
 
 void handleToFSensor()
 {
@@ -168,7 +169,8 @@ void loop()
 
   handleToFSensor();
 
-  if (now - lastSVGFetch >= SVG_INTERVAL) {
+  if (now - lastSVGFetch >= SVG_INTERVAL)
+  {
     lastSVGFetch = now;
     fetchSVGData();
   }
